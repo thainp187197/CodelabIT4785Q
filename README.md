58c2483536ae29a18a58286d57990ec0abf857a0
NGO PHU THAI 20187197 <thai.np187197@sis.hust.edu.vn>
2021-11-26 20:59:31 +0700

    Final.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Codelab môn Lập trình di động
+# CodelabIT4785Q
 
-Các codelab ở các branch.
+<img src=results/result1.png style="height: 40%; width: 40%;" />
+<img src=results/result2.png style="height: 40%; width: 40%;" />
+<img src=results/result3.png style="height: 40%; width: 40%;" />
--- app/src/main/java/com/thainp20187197/codelabit4785q/MainActivity.java
+++ app/src/main/java/com/thainp20187197/codelabit4785q/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,5 +15,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextName = findViewById(R.id.editTextName);
+        editTextMSSV = findViewById(R.id.editTextMSSV);
+        editTextBirthday = findViewById(R.id.editTextBirthday);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        checkBoxAgreement = findViewById(R.id.checkBoxAgreement);
+    }
+
+    EditText editTextName;
+    EditText editTextMSSV;
+    EditText editTextBirthday;
+    EditText editTextPhone;
+    EditText editTextEmail;
+    CheckBox checkBoxAgreement;
+
+
+    public void Submit(View view) {
+        if (!checkBoxAgreement.isChecked())
+        {
+            Toast.makeText(this, "Chưa đồng ý điều khoản", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String invalid = "Thiếu trường:";
+        String fields = "";
+        if (editTextName.getText().toString().isEmpty())
+        {
+            fields += " Họ tên;";
+        }
+        if (editTextMSSV.getText().toString().isEmpty())
+        {
+            fields += " MSSV;";
+        }
+        if (editTextBirthday.getText().toString().isEmpty())
+        {
+            fields += " Ngày sinh;";
+        }
+        if (editTextPhone.getText().toString().isEmpty())
+        {
+            fields += " Số điện thoại;";
+        }
+        if (editTextEmail.getText().toString().isEmpty())
+        {
+            fields += " Email;";
+        }
+
+        if ((invalid + fields).compareTo(invalid) == 0)
+        {
+            Toast.makeText(this, "Thành công", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, invalid + fields, Toast.LENGTH_SHORT).show();
+        }
     }
 }
