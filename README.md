035e6b9c8618ffc85a9fdcc2af626f388840c06d
NGO PHU THAI 20187197 <thai.np187197@sis.hust.edu.vn>
2022-01-18 00:04:00 +0700

    Final commit.

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 <img src=results/result1.png style="height: 40%; width: 40%;" />
 <img src=results/result2.png style="height: 40%; width: 40%;" />
 <img src=results/result3.png style="height: 40%; width: 40%;" />
+<img src=results/result4.png style="height: 40%; width: 40%;" />
+<img src=results/result5.png style="height: 40%; width: 40%;" />
+<img src=results/result6.png style="height: 40%; width: 40%;" />
+<img src=results/result7.png style="height: 40%; width: 40%;" />
+<img src=results/result8.png style="height: 40%; width: 40%;" />
--- app/src/main/java/com/thainp20187197/codelabit4785q/TextFileActivity.java
+++ app/src/main/java/com/thainp20187197/codelabit4785q/TextFileActivity.java
@@ -2,11 +2,41 @@ package com.thainp20187197.codelabit4785q;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class TextFileActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_file);
+
+        Bundle bundle = getIntent().getExtras();
+        String filePath = bundle.getString("filePath");
+        String fileName = bundle.getString("fileName");
+        String content = "";
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                content += line + "\n";
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            content = "File not found";
+        } catch (IOException e) {
+            e.printStackTrace();
+            content = "File can't be read";
+        }
+
+        ((TextView) findViewById(R.id.textFileContentTextView)).setText(content);
+        ((TextView) findViewById(R.id.textFileNameTextView)).setText(fileName);
     }
 }
