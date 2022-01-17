0302f8aab08edf7a884dcaf8294d92be8744e576
NGO PHU THAI 20187197 <thai.np187197@sis.hust.edu.vn>
2022-01-17 22:03:40 +0700

    Initial

---
+++ app/src/main/java/com/thainp20187197/codelabit4785q/FileAdapter.java
@@ -0,0 +1,67 @@
+package com.thainp20187197.codelabit4785q;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.List;
+
+public class FileAdapter extends BaseAdapter {
+    private List<File> files;
+
+    public FileAdapter(List<File> files) {
+        this.files = files;
+    }
+
+    @Override
+    public int getCount() {
+        return files.size() + 1;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return files.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View viewUser;
+        if (convertView == null)
+            viewUser = View.inflate(parent.getContext(), R.layout.file_list_view, null);
+        else
+            viewUser = convertView;
+
+        ImageView fileTypeIV = (ImageView) viewUser.findViewById(R.id.fileTypeImageView);
+        TextView fileNameTV = (TextView) viewUser.findViewById(R.id.fileNameTextView);
+        if (position == 0) {
+            fileTypeIV.setImageResource(R.drawable.ic_baseline_parent_folder_24);
+            fileNameTV.setText("..");
+        } else {
+            position--;
+            File file = (File) getItem(position);
+            String fileName = file.getName();
+
+            if (file.isFile()) {
+                if (fileName.contains(".txt"))
+                    fileTypeIV.setImageResource(R.drawable.ic_baseline_file_text_24);
+                else
+                    fileTypeIV.setImageResource(R.drawable.ic_baseline_file_24);
+            }
+            else {
+                fileTypeIV.setImageResource(R.drawable.ic_baseline_folder_24);
+            }
+
+            fileNameTV.setText(fileName);
+        }
+
+        return viewUser;
+    }
+}
--- app/src/main/java/com/thainp20187197/codelabit4785q/MainActivity.java
+++ app/src/main/java/com/thainp20187197/codelabit4785q/MainActivity.java
@@ -2,148 +2,45 @@ package com.thainp20187197.codelabit4785q;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Environment;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.thainp20187197.codelabit4785q.model.Address;
-import com.thainp20187197.codelabit4785q.model.Avatar;
-import com.thainp20187197.codelabit4785q.model.Company;
-import com.thainp20187197.codelabit4785q.model.User;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Serializable;
-import java.net.URL;
+import java.io.File;
 import java.util.ArrayList;
-
-import javax.net.ssl.HttpsURLConnection;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<User> users;
-    ListView userListView;
-    UserAdapter userAdapter;
+    List<File> files;
+    ListView fileListView;
+    File currentFile;
+    FileAdapter fileAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        userListView = findViewById(R.id.userListView);
-        userListView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Intent intent = new Intent(MainActivity.this, InfoActivity.class);
-                        User user = (User) parent.getItemAtPosition(position);
-                        intent.putExtra("user", user);
-                        startActivity(intent);
-                    }
-                }
-        );
-
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            new GetUsers().execute();
-        }
-
-        users = new ArrayList<>();
-        userAdapter = new UserAdapter(users);
-        userListView.setAdapter(userAdapter);
+        files = new ArrayList<>();
+        fileAdapter = new FileAdapter(files);
+        fileListView = findViewById(R.id.fileListView);
+        fileListView.setAdapter(fileAdapter);
+        ChangeDirectory(null);
     }
 
-    private void LoadUsers(String jsonResponse) {
-        users.clear();
-        try {
-            JSONArray jsonUsers = new JSONArray(jsonResponse);
-            for (int i = 0; true; i++) {
-                if (jsonUsers.isNull(i)) break;
-                JSONObject jsonUser = jsonUsers.getJSONObject(i);
-                JSONObject jsonAvatar = jsonUser.getJSONObject("avatar");
-                JSONObject jsonAddress = jsonUser.getJSONObject("address");
-                JSONObject jsonGeo = jsonAddress.getJSONObject("geo");
-                JSONObject jsonCompany = jsonUser.getJSONObject("company");
-                users.add(new User(
-                        jsonUser.getInt("id"),
-                        jsonUser.getString("name"),
-                        jsonUser.getString("username"),
-                        jsonUser.getString("email"),
-                        new Avatar(
-                                "https://lebavui.github.io" + jsonAvatar.getString("thumbnail"),
-                                "https://lebavui.github.io" + jsonAvatar.getString("photo")),
-                        new Address(
-                                jsonAddress.getString("street"),
-                                jsonAddress.getString("suite"),
-                                jsonAddress.getString("city"),
-                                jsonAddress.getString("zipcode"),
-                                jsonGeo.getString("lat"),
-                                jsonGeo.getString("lng")
-                        ),
-                        jsonUser.getString("phone"),
-                        jsonUser.getString("website"),
-                        new Company(
-                                jsonCompany.getString("name"),
-                                jsonCompany.getString("catchPhrase"),
-                                jsonCompany.getString("bs")
-                        )
-                ));
-            }
-        } catch (Exception ex)
-        {
-            Log.v("TAG", "Error!");
-        }
-        userAdapter.notifyDataSetChanged();
+    private void ChangeDirectory(File file) {
+        if (file == null) file = Environment.getExternalStorageDirectory();
+        currentFile = file;
+        files = Arrays.asList(Objects.requireNonNull(file.listFiles()));
+        fileAdapter.notifyDataSetChanged();
     }
 
-    private class GetUsers extends AsyncTask<Void, Void, String> {
-        ProgressDialog dialog;
-
-        @Override
-        protected void onPreExecute() {
-            dialog = new ProgressDialog(MainActivity.this);
-            dialog.setMessage("GET-ing data");
-            dialog.show();
-        }
-
-        @Override
-        protected String doInBackground(Void... voids) {
-            try {
-                URL url = new URL("https://lebavui.github.io/jsons/users.json");
-                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-
-                InputStream inputStream = connection.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                StringBuilder builder = new StringBuilder();
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                }
-                return builder.toString();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            Log.v("TAG", s);
-            LoadUsers(s);
-            dialog.dismiss();
-        }
+    @Override
+    public void onBackPressed() {
+        ChangeDirectory(currentFile.getParentFile());
     }
 }
